16e98c095f264152309c1098766a5bb26d9a015b
leon0625 <liangming.liu@foxmail.com>
2016-10-17 10:10:04 +0800

    修复git bash下，输出缓冲问题

--- pic/compress_img.py
+++ pic/compress_img.py
@@ -6,12 +6,14 @@
 import sys,tinify
 
 print("Now will compress image...")
+sys.stdout.flush()
 tinify.key = "7vXsNCs64jLDs2sPi-UO6Vx9_os1I__5"
 
 for i in range(1, len(sys.argv)):
-	print("compree image: " + sys.argv[i])
-	source = tinify.from_file(sys.argv[i])
-	source.to_file(sys.argv[i])
-	
+    print("compree image: " + sys.argv[i])
+    sys.stdout.flush()
+    source = tinify.from_file(sys.argv[i])
+    source.to_file(sys.argv[i])
+    
 print("All done")
 
